25fd2f4318be3add0efe82d7a5cad962649ce3c8
Saddam Lakho <62154427+saddamlakho@users.noreply.github.com>
2026-08-24 15:18:27 +0500

    update

--- lib/db.ts
+++ lib/db.ts
@@ -31,7 +31,26 @@
 
 
 
+// import { Pool } from "pg";
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: process.env.NODE_ENV === "production" 
+//     ? { rejectUnauthorized: false } 
+//     : false,
+// });
+
+// export default pool;
+
+
+
+
+
 import { Pool } from "pg";
+
+if (!process.env.DATABASE_URL) {
+  throw new Error("DATABASE_URL is not set in environment variables");
+}
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
